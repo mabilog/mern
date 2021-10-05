583e7db6817c4d8a246c5f38d88c28bf6c770771
angelo <g.angelo.m@gmail.com>
2021-10-05 14:44:26 -0400

    axios added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import Register from './components/Register'
 function App() {
   return (
-    <div>
-      test
-    </div>
+    <Router>
+      <div>
+        <Link to={'/'}>Home</Link> |
+        <Link to={'/login'}>Log In</Link> |
+        <Link to={'/register'}>Register</Link>
+      </div>
+      <Switch>
+        <Route exact path={'/register'} component={Register} />
+      </Switch>
+      
+    </Router>
   );
 }
 
---
+++ src/components/Register.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const Register = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const registerUser = (e) => {
+    e.preventDefault();
+
+    const data = {email,password}
+    axios.post(url: '', data)
+  }
+  return (
+    <form action='' onSubmit={(e) => registerUser(e)}>
+      <input 
+        type="email" 
+        placeholder="email" 
+        value={email} 
+        onChange={e => setEmail(e.target.value)}
+        /> <br/>
+      <input 
+        type="password" 
+        placeholder="password"  
+        value={password} 
+        onChange={e => setPassword(e.target.value)}
+        /> <br/>
+      <button type="submit">register</button>
+    </form>
+  )
+}
+
+export default Register
